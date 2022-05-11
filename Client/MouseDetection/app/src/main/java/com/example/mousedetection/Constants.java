cfa75d47f028c67a92f32cde7673cc22300df129
amardaus <amardaus@gmail.com>
2022-05-11 18:39:52 +0200

    dfcgvhbnjk

--- Client/MouseDetection/app/src/main/java/com/example/mousedetection/Constants.java
+++ Client/MouseDetection/app/src/main/java/com/example/mousedetection/Constants.java
@@ -3,6 +3,7 @@ package com.example.mousedetection;
 public class Constants {
     //public static String username = "User";
     //public static String serverAddress = "http://10.0.2.2:5000";
+    public static String cameraPort = "8081";
 
     public static String endpointGetAll = "/get_all";
     public static String endpointChangeToken = "/change_token/" ;
@@ -10,8 +11,12 @@ public class Constants {
     public static String endpointVerify = "/verify/";
     public static String endpointOpenTrap = "/open_trap/";
     public static String endpointImage = "/get_image/";
+    public static String endpointStatus = "/0/detection/status";
 
     public static String getURL(String ip, String port){
         return "http://" + ip + ":" + port;
+    }
+    public static String getCameraURL(String ip){
+        return "http://" + ip + ":" + cameraPort + "/stream.mjpeg";
     }
 }
